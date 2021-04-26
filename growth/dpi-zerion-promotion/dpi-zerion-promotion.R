cc55c4badda6c1a1ad2ed64804f4be61e10a73bd
JD Cook <josephcook36@gmail.com>
2021-04-26 15:05:22 -0500

    add index price feed

--- growth/dpi-zerion-promotion/dpi-zerion-promotion.R
+++ growth/dpi-zerion-promotion/dpi-zerion-promotion.R
@@ -1,3 +1,5 @@
 library(tidyverse)
 
 sells <- read_csv("dpi-zerion-sells-during-promotion.csv")
+buys <- read_csv("index_campaign_april_2021.csv")
+transfer_check <- read_csv("index_campaign_april_2021_who_did_transfers.csv")
