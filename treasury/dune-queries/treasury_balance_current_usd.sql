9d1ef186c66ff74b42dec156392c9c3a26291cbb
Jack Yin <jackyin0@gmail.com>
2021-05-13 15:56:17 -0400

    add generalized price CTE to pipeline

--- treasury/dune-queries/treasury_balance_current_usd.sql
+++ treasury/dune-queries/treasury_balance_current_usd.sql
@@ -1,5 +1,5 @@
 /*
-    query here: https://duneanalytics.com/queries/44425
+    query here: https://duneanalytics.com/queries/44939
 
     forked from https://duneanalytics.com/queries/22041/46378
 
@@ -11,14 +11,110 @@
     '\x26e316f5b3819264DF013Ccf47989Fb8C891b088' -- Community Treasury Year 1 Vesting
     )    
     
-    -- Target tokens:
-    WHEN contract_address = '\x0954906da0bf32d5479e25f46056d22f08464cab' THEN 'INDEX'
-    WHEN contract_address = '\x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b' THEN 'DPI'
-    when contract_address = '\xaa6e8127831c9de45ae56bb1b0d4d4da6e5665bd' then 'FLI-ETH2X'
 
 */
 
-WITH wallets AS (
+-- Start Generalized Price Feed block - see generalized_price_feed.sql
+WITH prices_usd AS (
+
+    SELECT
+        date_trunc('day', minute) AS dt
+        , symbol
+        , AVG(price) AS price
+    FROM prices.usd
+    WHERE symbol in ('INDEX', 'DPI', 'MVI', 'ETH2x-FLI', 'BTC2x-FLI')
+    GROUP BY 1,2
+)
+    
+, swaps AS (
+    -- Uniswap price feed
+    SELECT
+        date_trunc('hour', sw."evt_block_time") AS hour
+        , case 
+            when contract_address = '\x3452A7f30A712e415a0674C0341d44eE9D9786F9' then 'INDEX'
+            when contract_address = '\x4d5ef58aac27d99935e5b6b4a6778ff292059991' then 'DPI'
+            when contract_address = '\x4d3C5dB2C68f6859e0Cd05D080979f597DD64bff' then 'MVI'
+            when contract_address = '\xf91c12dae1313d0be5d7a27aa559b1171cc1eac5' then 'ETH2x-FLI'
+        end as symbol
+        , ("amount0In" + "amount0Out")/1e18 AS a0_amt
+        , ("amount1In" + "amount1Out")/1e18 AS a1_amt
+    FROM uniswap_v2."Pair_evt_Swap" sw
+    WHERE contract_address in ( '\x3452A7f30A712e415a0674C0341d44eE9D9786F9' -- liq pair addresses I am searching the price for
+                                , '\x4d5ef58aac27d99935e5b6b4a6778ff292059991'
+                                , '\x4d3C5dB2C68f6859e0Cd05D080979f597DD64bff' 
+                                , '\xf91c12dae1313d0be5d7a27aa559b1171cc1eac5' )
+        AND sw.evt_block_time >= '2020-09-10'
+
+    union all
+    
+    -- Sushi price feed
+    SELECT
+        date_trunc('hour', sw."evt_block_time") AS hour
+        , 'BTC2x-FLI' as symbol
+        , ("amount0In" + "amount0Out")/1e18 AS a0_amt
+        , ("amount1In" + "amount1Out")/1e8 AS a1_amt
+    FROM sushi."Pair_evt_Swap" sw
+    WHERE contract_address = '\x164fe0239d703379bddde3c80e4d4800a1cd452b' -- liq pair address I am searching the price for
+        AND sw.evt_block_time >= '2021-05-11'
+
+)
+, swap_a1_prcs AS (
+    SELECT 
+        avg(price) a1_prc
+        , date_trunc('hour', minute) AS hour
+    FROM prices.usd
+    WHERE minute >= '2020-09-10'
+        AND contract_address ='\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' --weth as base asset
+    GROUP BY 2               
+)
+, uni_hours AS (
+    SELECT generate_series('2020-09-10 00:00:00'::timestamp, date_trunc('hour', NOW()), '1 hour') AS hour -- Generate all days since the first contract
+)
+, uni_temp AS (
+    SELECT
+        h.hour
+        , s.symbol
+        , COALESCE(AVG((s.a1_amt/s.a0_amt)*a.a1_prc), NULL) AS usd_price
+        , COALESCE(AVG(s.a1_amt/s.a0_amt), NULL) as eth_price
+        -- a1_prcs."minute" AS minute
+    FROM uni_hours h
+    LEFT JOIN swaps s ON h."hour" = s.hour 
+    LEFT JOIN swap_a1_prcs a ON h."hour" = a."hour"
+    GROUP BY 1,2
+) 
+, swap_feed AS (
+    SELECT
+        hour
+        , symbol
+        , (ARRAY_REMOVE(ARRAY_AGG(usd_price) OVER (PARTITION BY symbol ORDER BY hour), NULL))[COUNT(usd_price) OVER (PARTITION BY symbol ORDER BY hour)] AS usd_price
+        , (ARRAY_REMOVE(ARRAY_AGG(eth_price) OVER (PARTITION BY symbol ORDER BY hour), NULL))[COUNT(eth_price) OVER (PARTITION BY symbol ORDER BY hour)] AS eth_price
+    FROM uni_temp
+)
+, swap_price_feed AS ( -- only include the uni feed when there's no corresponding price in prices_usd
+
+    SELECT
+        date_trunc('day', hour) AS dt
+        , u.symbol
+        , AVG(usd_price) AS price
+    FROM swap_feed u
+    left join prices_usd p on date_trunc('day', u.hour) = p.dt
+        and u.symbol = p.symbol
+    WHERE p.dt is null
+        AND usd_price IS NOT NULL
+    GROUP BY 1, 2
+
+)
+, prices AS (
+    SELECT *
+    FROM prices_usd
+    where dt > '2020-10-06'
+    UNION ALL
+    SELECT *
+    FROM swap_price_feed
+    where dt > '2020-10-06'
+)
+-- End price feed block - output is CTE "prices"
+, wallets AS (
     SELECT 'INDEX' AS org
         , '\x9467cfadc9de245010df95ec6a585a506a8ad5fc'::bytea AS address
         , 'Treasury Wallet' AS wallet
@@ -40,40 +136,13 @@ WITH wallets AS (
     WHERE address IN (SELECT address FROM wallets)
     AND TYPE = 'create'
 )
-
 , days AS (
     SELECT 
         generate_series(MIN(day), date_trunc('day', NOW()), '1 day') AS day -- Generate all days since the first contract
     FROM creation_days
 )
-
-, prices as ( 
-    -- using dex price approximations table only containes FLI-ETH2X
-    select date_trunc('day',hour) as day
-        , contract_address
-        , case when contract_address = '\x0954906da0bf32d5479e25f46056d22f08464cab' THEN 'INDEX'
-            WHEN contract_address = '\x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b' THEN 'DPI'
-            when contract_address = '\xaa6e8127831c9de45ae56bb1b0d4d4da6e5665bd' then 'FLI-ETH2X'
-            end as symbol
-        , avg(median_price) as price
-    from dex.view_token_prices
-    where contract_address in ('\xaa6e8127831c9de45ae56bb1b0d4d4da6e5665bd') -- FLI-ETH2X,
-    group by 1,2,3
-    union
-    -- prices.usd only contains price data for INDEX and DPI
-    select date_trunc('day', minute)
-        , contract_address
-        , symbol
-        , avg(price) as price
-    from prices.usd
-    where contract_address in ('\x0954906da0bf32d5479e25f46056d22f08464cab' -- INDEX
-                        , '\x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b') -- DPI
-    group by 1,2,3
-
-)
-
 , transfers AS (
---ERC20 Tokens
+    --ERC20 Tokens
     SELECT
         date_trunc('day', evt_block_time) AS day,
         "from" AS address,
@@ -84,7 +153,7 @@ WITH wallets AS (
     AND evt_block_time >= (SELECT min(day) FROM creation_days)
     GROUP BY 1,2,3
     
-UNION ALL
+    UNION ALL
 
     SELECT
         date_trunc('day', evt_block_time) AS day,
@@ -128,24 +197,24 @@ UNION ALL
     GROUP BY 1,2 --,3
     ORDER BY 1,2 --,3
 )
-
 , usd_value_all_days as (
-SELECT
-    b.day,
---    b.address,
---    w.wallet,
---    w.org,
-    b.contract_address,
-    p.symbol AS token,
-    b.balance,
-    p.price,
-    b.balance * coalesce(p.price,0) AS usd_value
-    , rank() over (order by b.day desc)
-FROM balances_all_days b
-LEFT OUTER JOIN prices p ON b.contract_address = p.contract_address AND b.day = p.day
--- LEFT OUTER JOIN wallets w ON b.address = w.address
-ORDER BY usd_value DESC
-LIMIT 10000
+    SELECT
+        b.day,
+    --    b.address,
+    --    w.wallet,
+    --    w.org,
+        b.contract_address,
+        p.symbol AS token,
+        b.balance,
+        p.price,
+        b.balance * coalesce(p.price,0) AS usd_value
+        , rank() over (order by b.day desc)
+    FROM balances_all_days b
+    left join erc20.tokens t on b.contract_address = t.contract_address
+    LEFT OUTER JOIN prices p ON t.symbol = p.symbol AND b.day = p.dt
+    -- LEFT OUTER JOIN wallets w ON b.address = w.address
+    ORDER BY usd_value DESC
+    LIMIT 10000
 )
 select contract_address
     , token
