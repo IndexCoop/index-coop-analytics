1b5d678edd18c3477cc9cb46f006a94dbfbd1965
Lorengo <abcd1023450@163.com>
2021-06-07 21:58:39 +0800

    add dpi nav vs market price:all time,30days,3days

--- product/navs-vs-market-prices/DPI nav vs market price all time hourly.sql
+++ product/navs-vs-market-prices/DPI nav vs market price all time hourly.sql
@@ -1,5 +1,5 @@
 /* dune query here: https://duneanalytics.com/queries/53567
-This query compares DPI NAV and it's market price.
+This query compares all time DPI NAV and it's market price hourly.
 How it works?
 1.figures out the composition of per DPI each hour,then times the price of each compostion at that time ,adds them up gets the dpi nav
 2.compares dpi nav with dpi market price 
@@ -11,15 +11,15 @@ with DPI_mint_amount as
     select
         *
     from (
-    select
-        "evt_tx_hash",
-        "evt_block_time",
-        date_trunc('hour', "evt_block_time") as hour_date,
-        "_quantity"/1e18 as DPI_amount,
-        row_number() over(partition by date_trunc('hour', "evt_block_time") order by  "evt_block_time" desc) as rnb
-    from setprotocol_v2."BasicIssuanceModule_evt_SetTokenIssued"
-    where "_setToken" IN ('\x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b')
-    ) x 
+          select
+              "evt_tx_hash",
+              "evt_block_time",
+              date_trunc('hour', "evt_block_time") as hour_date,
+              "_quantity"/1e18 as DPI_amount,
+              row_number() over(partition by date_trunc('hour', "evt_block_time") order by  "evt_block_time" desc) as rnb
+          from setprotocol_v2."BasicIssuanceModule_evt_SetTokenIssued"
+          where "_setToken" IN ('\x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b')
+         ) x 
     where x.rnb = 1
 ),
 
@@ -34,15 +34,14 @@ DPI_composition as
         a."value"/10^b."decimals"/c.DPI_amount as asset_amount,
         c.DPI_amount,
         b.symbol
-    from
-    (
+    from (
           select
               *
           from erc20."ERC20_evt_Transfer"
           where "evt_tx_hash" in (select "evt_tx_hash" from DPI_mint_amount )
           and "to" = '\x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b'
           and  "contract_address" != '\x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b'
-    )  a
+         )  a
     left join erc20."tokens" b
     on a."contract_address" = b."contract_address"
     left join DPI_mint_amount c
@@ -58,14 +57,14 @@ DPI_composition_in_out_time as
         in_index_time,
         case when last_time < last_mint_time then last_time else now() end as out_index_time
     from (
-         select
-            asset_address,
-            min(hour_date) as in_index_time,
-            max(hour_date) as last_time,
-            (select max(hour_date) from DPI_composition ) as last_mint_time
-        from DPI_composition 
-        group by asset_address
-        ) x 
+          select
+              asset_address,
+              min(hour_date) as in_index_time,
+              max(hour_date) as last_time,
+              (select max(hour_date) from DPI_composition ) as last_mint_time
+          from DPI_composition 
+          group by asset_address
+         ) x 
 
 ),
 
@@ -108,21 +107,21 @@ DPI_composition_hourly_NAV as
         first_value(asset_amount) over (partition by contract_address,grp_asset order by price_time ) as asset_correct,
         price*first_value(asset_amount) over (partition by contract_address,grp_asset order by price_time ) as asset_NAV
     from (
-    select
-        a.contract_address,
-        a.price_time,
-        a.price,
-        b.asset_amount,
-        sum(case when b.asset_amount is not null then 1 end) over (partition by a.contract_address order by a.price_time) as grp_asset
-    from DPI_composition_price a
-    left join DPI_composition b
-    on a.contract_address = b.asset_address
-    and a.price_time = b.hour_date
-    left join DPI_composition_in_out_time c
-    on a.contract_address = c.asset_address
-    where a.price_time >= c.in_index_time
-      and a.price_time <= c.out_index_time
-    ) x
+          select
+              a.contract_address,
+              a.price_time,
+              a.price,
+              b.asset_amount,
+              sum(case when b.asset_amount is not null then 1 end) over (partition by a.contract_address order by a.price_time) as grp_asset
+          from DPI_composition_price a
+          left join DPI_composition b
+          on a.contract_address = b.asset_address
+          and a.price_time = b.hour_date
+          left join DPI_composition_in_out_time c
+          on a.contract_address = c.asset_address
+          where a.price_time >= c.in_index_time
+            and a.price_time <= c.out_index_time
+         ) x
 )
 ,
 
