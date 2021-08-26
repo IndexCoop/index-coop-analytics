85ced4615e245be6ce09ef8a55307be2c87b5474
Jack Yin <jackyin0@gmail.com>
2021-08-26 12:54:43 -0400

    changed index directory schema

--- governance/dune-queries/index_wallets_ranked.sql
+++ governance/dune-queries/index_wallets_ranked.sql
@@ -2,10 +2,10 @@
 
 with token_balances as (
   select b.wallet_address
-    , case when nv.wallet_address is not null then 'Nonvoting' else 'Voting' end as wallet_type
+    , coalesce(nv.wallet_type, 'Voting') as wallet_type
     , b.amount
   from erc20."view_token_balances_latest" b
-  left join dune_user_generated.index_nonvoting_addresses nv on b.wallet_address = nv.wallet_address
+  left join dune_user_generated.index_addresses nv on b.wallet_address = nv.wallet_address
   where b.token_address = '\x0954906da0Bf32d5479e25f46056d22f08464cab' -- INDEX token address
   and b.wallet_address <> '\x0000000000000000000000000000000000000000'
   and amount > 0
