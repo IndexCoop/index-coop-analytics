1ffb80a68a0bf3fe815a6b84689b3e78e65c6447
Anthony Bowman <83092494+anthonymbowman@users.noreply.github.com>
2022-10-25 10:44:44 -0400

    Update for performance

--- abstractions/price/view_indexcoop_prices_daily.sql
+++ abstractions/price/view_indexcoop_prices_daily.sql
@@ -1,71 +1,32 @@
--- Ethereum - 
+-- Ethereum - https://dune.xyz/queries/504213
 
+-- drop view if exists dune_user_generated.indexcoop_prices_daily
 CREATE OR REPLACE VIEW 
 dune_user_generated.indexcoop_prices_daily as
 
-(
-with
-
-eligible_pairs as (
-      select 'WETH' as symbol
-union select 'WMATIC'
-union select 'USDC'
-),
-
-trades as (
-select
-    date_trunc('minute', a.block_time) as minute,
-    b.symbol as index_symbol,
-    a.token_b_symbol as base_symbol,
-    a.token_b_amount / nullif((a.token_a_amount_raw/1e18),0) as relative_price
-from        dex."trades" a
-inner join  dune_user_generated."indexcoop_tokens" b on a.token_a_address = b.token_address
-inner join  eligible_pairs ep on a.token_b_symbol = ep.symbol
-where a.block_time > '2022-03-13' -- this is the last cache date
-
-union all
-
-select
-    date_trunc('minute', a.block_time) as minute,
-    b.symbol as index_symbol,
-    a.token_a_symbol as base_symbol,
-    a.token_a_amount / nullif((a.token_b_amount_raw/1e18),0) as relative_price
-from        dex."trades" a
-inner join  dune_user_generated."indexcoop_tokens" b on a.token_b_address = b.token_address
-inner join  eligible_pairs ep on a.token_a_symbol = ep.symbol
-where a.block_time > '2022-03-13'
-),
-
-base_prices as (
-select
-    p.symbol,
-    p.minute,
-    p.price
-from        prices.usd p
-inner join  eligible_pairs ep on p.symbol = ep.symbol
-where       minute >= '2022-03-13' -- Cache date
-),
-
-usd_trades as (
-select
-    a.minute,
-    a.index_symbol as symbol,
-    a.relative_price * b.price as index_usd
-from        trades a
-left join   base_prices b on a.minute = b.minute and a.base_symbol = b.symbol
-),
-
-price_feed as (
 select
     symbol,
-    date_trunc('day', minute) as day,
-    percentile_cont(.5) within group (order by index_usd) as price -- faster to get the VWAP
-from        usd_trades
-group by    day, symbol
+    day,
+    price
+from    (
+        select
+            symbol,
+            day,
+            round(price,2) as price
+        from    (
+                select
+                    date_trunc('day', p.hour) as day,
+                    t.symbol,
+                    p.median_price as price,
+                    row_number() over (partition by t.symbol, date_trunc('day', p.hour) order by p.hour desc) as rnb
+                from        prices.prices_from_dex_data p
+                inner join  dune_user_generated."indexcoop_tokens" t on t.token_address = p.contract_address
+                where       p.hour >= '2022-10-01'
+                ) t0
+        where       t0.rnb = 1
+        
+        union
+        
+        select * from dune_user_generated.indexcoop_prices_daily_cached
+        ) t
 order by    symbol, day
-)
-
-select * from price_feed
-union all
-select * from dune_user_generated.indexcoop_prices_daily_cached
-)
