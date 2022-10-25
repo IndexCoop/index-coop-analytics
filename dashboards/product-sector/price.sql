4bd435c0ee963d7c44357de57f95a29706ef7896
Anthony Bowman <83092494+anthonymbowman@users.noreply.github.com>
2022-10-25 10:52:17 -0400

    Update price.sql

--- dashboards/product-sector/price.sql
+++ dashboards/product-sector/price.sql
@@ -8,15 +8,8 @@ hour of each day in the range.
 select
     day as "Day",
     price as "Price"
-from    (
-        select
-            date_trunc('day', hour) as day,
-            median_price as price,
-            row_number() over (partition by date_trunc('day', hour) order by hour desc) as rnb
-        from    prices.prices_from_dex_data
-        where   contract_address = (select token_address from dune_user_generated."indexcoop_tokens" where symbol = '{{Index Coop Sector Token:}}')
-        and     date_trunc('day', hour) > date_trunc('day', least('{{End Date:}}', now())) - interval '{{Trailing Days:}} days'
-        and     date_trunc('day', hour) <= date_trunc('day', least('{{End Date:}}', now()))
-        ) t
-where       t.rnb = 1
+from    dune_user_generated.indexcoop_prices_daily 
+where   symbol = '{{Index Coop Sector Token:}}'
+and     day > date_trunc('day', least('{{End Date:}}', now())) - interval '{{Trailing Days:}} days'
+and     day <= date_trunc('day', least('{{End Date:}}', now()))
 order by    day desc
