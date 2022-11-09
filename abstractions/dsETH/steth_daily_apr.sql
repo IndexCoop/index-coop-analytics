562dcb6100e5e388de668669eae76aa81e92d21e
Anthony Bowman <83092494+anthonymbowman@users.noreply.github.com>
2022-11-09 12:27:37 -0500

    Update steth_daily_apr.sql

--- abstractions/dsETH/steth_daily_apr.sql
+++ abstractions/dsETH/steth_daily_apr.sql
@@ -1,18 +1,22 @@
 -- https://dune.com/queries/1548421
 
-create or replace view dune_user_generated.steth_daily_apr as
+create or replace view dune_user_generated.wsteth_updates as
+
 select
-    day,
-    apr
+    pd_start,
+    pd_length,
+    365 * (rate_increase / pd_length) as pd_apr,
+    pd_start_nav
 from    (
         select
-            day,
-            lead(apr,1) over (order by day) as apr
+            time as pd_start,
+            extract(epoch from lead(time,1) over (order by time) - time)/60/60/24 as pd_length,
+            (lead(er,1) over (order by time) / er) - 1 as rate_increase,
+            er as pd_start_nav
         from    (
-                select 
-                    date_trunc('day', evt_block_time) as day,
-                    365 * ("postTotalPooledEther" / "totalShares" / (lag("postTotalPooledEther" / "totalShares", 1) over (order by evt_block_time)) - 1 ) AS apr
+                select
+                    "evt_block_time" as time,
+                    "postTotalPooledEther" / "totalShares" as er
                 from    lido."LidoOracle_evt_PostTotalShares"
                 ) t0
         ) t1
-where   apr is not null
